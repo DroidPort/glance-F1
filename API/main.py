b6c9964299a7c5fadc67b1a47801c3f6c439f249
DroidPort <droidport@proton.me>
2026-06-06 16:24:54 +1000

    Update main.py

--- API/main.py
+++ API/main.py
@@ -7,6 +7,7 @@ from API_Endpoints.constructors_cleaner import router as constructors_cleaner
 from API_Endpoints.drivers_cleaner import router as drivers_cleaner
 from API_Endpoints.map.router import router as map_router
 from API_Endpoints.last_race_cleaner import router as last_race_cleaner
+from API_Endpoints.session_results_cleaner import router as session_results_router
 
 app = FastAPI()
 
@@ -20,3 +21,4 @@ app.include_router(last_race_cleaner, prefix="/f1/last_race")
 app.include_router(constructors_cleaner, prefix="/f1/constructors_standings")
 app.include_router(drivers_cleaner, prefix="/f1/drivers_standings")
 app.include_router(map_router, prefix="/f1/next_map")
+app.include_router(session_results_router, prefix="/f1/session_results")
